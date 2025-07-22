12d4b1c8c46b3ae1c2a88cb6922fd9069ade19f9
Nguyễn Tuấn Anh <anhnt15cp@gmail.com>
2025-07-22 16:41:53 +0700

    Add xcodeproj and source files

---
+++ Submodule/Submodule.h
@@ -0,0 +1,18 @@
+//
+//  Submodule.h
+//  Submodule
+//
+//  Created by Nguyễn Tuấn anh on 22/7/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Submodule.
+FOUNDATION_EXPORT double SubmoduleVersionNumber;
+
+//! Project version string for Submodule.
+FOUNDATION_EXPORT const unsigned char SubmoduleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Submodule/PublicHeader.h>
+
+
